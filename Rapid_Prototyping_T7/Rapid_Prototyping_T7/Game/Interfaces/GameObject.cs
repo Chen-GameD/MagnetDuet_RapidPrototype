27f3cd8c9a0bebdcc949bd0cbc8438d5bffdf512
Hans <hnsgunther@gmail.com>
2021-08-27 11:25:02 -0600

    Refactor GameObject interface to abstract class
    Added important fields to GameObject like position and sprite information

---
+++ Rapid_Prototyping_T7/Rapid_Prototyping_T7/Game/Interfaces/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Rapid_Prototyping_T7.Game
+{
+    abstract class GameObject
+    {
+        protected Texture2D sprite;
+        public Texture2D Sprite
+        {
+            get { return sprite; }
+            set { sprite = value; }
+        }
+
+        protected Vector2 position;
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public abstract void Initialize();
+
+        public abstract void LoadContent(ContentManager content);
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
