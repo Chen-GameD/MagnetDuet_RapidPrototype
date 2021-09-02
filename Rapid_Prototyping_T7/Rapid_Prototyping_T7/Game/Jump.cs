18da1949ff0d77039b2ddc5c6f36bebd8f9caf41
Hans <hnsgunther@gmail.com>
2021-09-02 11:15:20 -0600

    Player starts with no battery

--- Rapid_Prototyping_T7/Rapid_Prototyping_T7/Game/Jump.cs
+++ Rapid_Prototyping_T7/Rapid_Prototyping_T7/Game/Jump.cs
@@ -19,7 +19,7 @@ namespace Rapid_Prototyping_T7.Game
         public static float acceleration_gravity = 10f;
         public static float distance_decay_exponant = 1.4f;
 
-        public static float battery_duration = 1f;
+        public static float battery_duration = 0f;
         public static float battery_getCollected = 1f;
         public static float super_jump_force_multiplyer = 1.5f;
 
